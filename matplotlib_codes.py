18208322d0974b689891d054eda90d6665360dae
DAYACHAND PRAJAPAT <157813327+DAYACHAND-PRAJAPAT@users.noreply.github.com>
2025-09-05 20:02:30 +0530

    Add files via upload

---
+++ matplotlib_codes.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+# # Basic plot
+# x = np.array([23,67,69,45,60])
+# y = x ** 2
+# plt.plot(x,y)
+# plt.show()
+
+
+# # Straight plot
+# x = [23,67,69,45,60]
+# x.sort()
+# data = np.array(x)
+# y = data ** 2
+# plt.plot(data,y)
+# plt.show()
+
+# Plotting a stock movement graph
+# days = np.arange(1,11)
+# price = [120,125,167,185,134,145,134,178,157,156]
+# plt.plot(days,price,marker='o',color='green',label='Stock Prices')
+# plt.xlabel('Days')
+# plt.ylabel('Prices ($)')
+# plt.legend()
+# plt.show()
